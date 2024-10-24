b313fb302c9fb83aae5e79d62709944559e73eea
Ishika <116502498+Ishika54217@users.noreply.github.com>
2024-10-24 17:37:10 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ProductsPage from './pages/ProductsPage';
+import './styles/style.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
