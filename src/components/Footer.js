c0b592c0c2cc262e8eb6c49553fec888d49cd215
Ishika <116502498+Ishika54217@users.noreply.github.com>
2024-10-24 17:38:23 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__copy">
+        © 2024 Hayday Info. All rights reserved.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
