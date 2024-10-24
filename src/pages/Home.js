9190074472290ce3d537d32416fe4547e9c69886
Ishika <116502498+Ishika54217@users.noreply.github.com>
2024-10-24 17:39:17 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <section className="home section">
+      <div className="container">
+        <h2 className="section__title">Welcome to Hayday Info</h2>
+        <span className="section__subtitle">Your guide to everything Hayday</span>
+        <div className="home__content">
+          {/* Add your home content here */}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
